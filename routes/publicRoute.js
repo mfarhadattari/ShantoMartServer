87c7854ed8a7ae5ee3940bb0af610d5be6f8a1b7
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-07-08 10:52:24 +0600

    All products, products details, new-products api implementation

--- routes/publicRoute.js
+++ routes/publicRoute.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const router = express.Router();
 
+// ! Add to cart API
 router.post("/add-to-cart", async (req, res) => {
   const cartCollection = req.cartCollection;
   const data = req.body;
@@ -18,6 +20,33 @@ router.post("/add-to-cart", async (req, res) => {
   }
   const result = await cartCollection.insertOne(data);
   return res.send(result);
+});
+
+// ! All Products API
+router.get("/products", async (req, res) => {
+  const productCollection = req.productCollection;
+  const products = await productCollection.find().toArray();
+  res.send(products);
+});
+
+// ! ------------------ Products Details ---------------
+router.get("/products/:id", async (req, res) => {
+  const productCollection = req.productCollection;
+  const id = req.params.id;
+  const product = await productCollection.findOne({ _id: new ObjectId(id) });
+  res.send(product);
+});
+
+// ! ---------------- New Products --------------------
+router.get("/new-products", async (req, res) => {
+  const productCollection = req.productCollection;
+  const newProducts = await productCollection
+    .find({})
+    .sort({ timeDate: -1 })
+    .limit(6)
+    .toArray();
+
+  res.send(newProducts);
 });
 
 module.exports = router;
