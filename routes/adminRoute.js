03c4b9e6645a4ea9fd3047b448b81cf4d3d6d91b
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-07-08 19:00:17 +0600

    update product api implementation

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { ObjectId } = require("mongodb");
 const router = express.Router();
 
-
 // ! ------------------ Products ---------------
 router.get("/products", async (req, res) => {
   const productCollection = req.productCollection;
@@ -23,6 +22,20 @@ router.post("/add-product", async (req, res) => {
   const productCollection = req.productCollection;
   const data = req.body;
   const result = await productCollection.insertOne(data);
+  res.send(result);
+});
+
+// ! ----------- Update Product ---------------
+router.patch("/update-product/:id", async (req, res) => {
+  const productCollection = req.productCollection;
+  const query = { _id: new ObjectId(req.params.id) };
+  const data = req.body;
+  const updateDoc = {
+    $set: {
+      ...data,
+    },
+  };
+  const result = await productCollection.updateOne(query, updateDoc);
   res.send(result);
 });
 
