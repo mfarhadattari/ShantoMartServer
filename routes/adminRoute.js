2525cd069a9cbfb0844b29c13bdb0e1352104cc3
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-07-08 16:57:58 +0600

    add product api implementation

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/products", async (req, res) => {
   const productCollection = req.productCollection;
   const products = await productCollection.find().toArray();
-  res.send(products);
+  res.send(products.reverse());
 });
 
 // ! ------------------ Delete Product ---------------
@@ -15,6 +15,14 @@ router.delete("/delete-product/:id", async (req, res) => {
   const productCollection = req.productCollection;
   const id = req.params.id;
   const result = await productCollection.deleteOne({ _id: new ObjectId(id) });
+  res.send(result);
+});
+
+// ! ------------------ Add to Product ---------------
+router.post("/add-product", async (req, res) => {
+  const productCollection = req.productCollection;
+  const data = req.body;
+  const result = await productCollection.insertOne(data);
   res.send(result);
 });
 
