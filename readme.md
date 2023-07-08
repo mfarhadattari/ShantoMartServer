cbf977ce11dc4c2b5e8053d46717489791b7090c
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-07-08 14:50:41 +0600

    Admin route all product and delete product api implementation

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # ShantoMart
 ### An E-commerce Site
+### https://shanto-mart-server.vercel.app/
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const router = express.Router();
 
 
+// ! ------------------ Products ---------------
+router.get("/products", async (req, res) => {
+  const productCollection = req.productCollection;
+  const products = await productCollection.find().toArray();
+  res.send(products);
+});
 
+// ! ------------------ Delete Product ---------------
+router.delete("/delete-product/:id", async (req, res) => {
+  const productCollection = req.productCollection;
+  const id = req.params.id;
+  const result = await productCollection.deleteOne({ _id: new ObjectId(id) });
+  res.send(result);
+});
 
 module.exports = router;
